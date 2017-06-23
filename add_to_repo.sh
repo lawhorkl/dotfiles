dd0dd355476002c70203b0009344017255ca584d
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2017-06-23 17:38:20 -0400

    add additional setup scripts

---
+++ add_to_repo.sh
@@ -0,0 +1,12 @@
+rm .bash_profile
+mv ~/.bash_profile .
+rm .bashrc
+mv ~/.bashrc .
+rm -rf .vim/
+mv ~/.vim .vim/
+rm .vimrc
+mv ~/.vimrc .
+rm .viminfo
+mv ~/.viminfo .
+rm .tmux.conf
+mv ~/.tmux.conf .
---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ghuntley/terminator-solarized.git
+cd terminator-solarized
+mkdir -p ~/.config/terminator/
+touch ~/.config/terminator/config
+# if you want to replace current config:
+cp config ~/.config/terminator
