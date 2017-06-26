1a4d42cf35f2a33188b146c54ee4ee4bc14aa4eb
Kyle Lawhorn <lawhorkl@mail.uc.edu>
2017-06-25 20:43:45 -0400

    replace terminator config with yakuake and add colors

--- copy_files.sh
+++ copy_files.sh
@@ -1,9 +1,10 @@
-mkdir ~/.vim && cp -rf .vim/ ~/.vim
+mkdir ~/.vim && cp .vim/* ~/.vim
+mkdir ~/yakuake-solarized && cp yakuake_solarized/* ~/yakuake-solarized
 cp .bash_profile ~/.bash_profile
 cp .bashrc ~/.bashrc
 cp .git-completion.bash ~/.git-completion.bash
 cp .git-prompt.sh ~/.git-prompt.sh
-cp .tmux.conf ~/.tmux.conf
 cp .viminfo ~/.viminfo
 cp .vimrc ~/.vimrc
 
+cd ~/yakuake-solarized && bash install.sh
